ded8225dc8035e8577882a2855dfd8d4abcc370f
Jeffrey O Adams <frexjeff@gmail.com>
2021-02-19 17:26:16 +0000

    Merge origin/master
    Conflicts:
    	src/bt/bitsmartmini/ui/CatalogController.java
    	src/bt/bitsmartmini/ui/ItemCart.fxml

--- src/bt/bitsmartmini/utils/PrintReport.java
+++ src/bt/bitsmartmini/utils/PrintReport.java
@@ -25,7 +25,9 @@ import bt.bitsmartmini.reportmodel.StockReorderReportModel;
 import bt.bitsmartmini.reportmodel.StockReportModel;
 import bt.bitsmartmini.ui.LoginController;
 import bt.bitsmartmini.ui.MainAppController;
+import java.io.File;
 import java.io.FileInputStream;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import lxe.utility.date.DateUtil;
@@ -59,23 +61,12 @@ public class PrintReport extends JFrame {
     public void showReceiptReport(String id) throws JRException, ClassNotFoundException, SQLException, IOException {
         ReceiptBL rbl = new ReceiptBL();
         CustomerBL cb = new CustomerBL();
-//        ReturnBL rb = new ReturnBL();
-        String logo = MainAppController.B.getBLogo();
         Receipt r = rbl.getReciptbyCode(Integer.parseInt(id));
         param = setBasiParam();
         Customers c = cb.getCustomersBySales(r.getSalesId().getSalesId());
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/TerminalReceiptReport.jasper");
-        // System.out.println("LOGO: " + logo);
-        BufferedImage image;
-        FileInputStream ifile = new FileInputStream(logo);
-        //BufferedImage image = ImageIO.read(getClass().getResourceAsStream(logo));
-        if (logo != null) {
-            image = ImageIO.read(ifile);
-        } else {
-            image = ImageIO.read(ifile);
-            image = ImageIO.read(getClass().getResourceAsStream(MainAppController.B.getBLogo()));
-        }
-        //System.out.println("IMG: " + MainAppController.B.getBLogo());
+        File f = new File(MainAppController.B.getBLogo());
+        BufferedImage image = ImageIO.read(f);
         BufferedImage imageback = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logoback.png"));
         param.put("LOGO", image);
         param.put("LOGO1", imageback);
@@ -85,13 +76,9 @@ public class PrintReport extends JFrame {
         param.put("MODE", r.getPayMode());
         param.put("CUSTOMER", c.getFullname());
         param.put("CUSTMOBILE", c.getMobile());
-        //param.put("CUSTMOBILE", c.getMobile());
         param.put("REFUND_POLICY", r.getReturnPolicy());
         param.put("SOLDBY", r.getUsers().getUserid().toString());
         ReceiptReportModel sm = new ReceiptReportModel(r.getSalesId().getSalesId());
-        //sm.salesId = r.getSalesId().getSalesId();
-        //Thread t = new Thread(sm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(sm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
@@ -108,7 +95,8 @@ public class PrintReport extends JFrame {
             InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/SalesReceiptReport.jasper");
             // Fields for report
             param = setBasiParam();
-            BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logo.png"));
+            File f = new File(MainAppController.B.getBLogo());
+            BufferedImage image = ImageIO.read(f);
             param.put("LOGO", image);
             param.put("SD", DateUtil.format3(start));
             param.put("ED", DateUtil.format3(end));
@@ -118,11 +106,9 @@ public class PrintReport extends JFrame {
             JRViewer viewer = new JRViewer(print);
             viewer.setOpaque(true);
             viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
             this.add(viewer);
             this.setSize(dim);
             this.setVisible(true);
-//            System.out.print("Done!");
         } catch (Exception ex) {
             Logger.getLogger(PrintReport.class.getName()).log(Level.SEVERE, null, ex);
 
@@ -131,36 +117,31 @@ public class PrintReport extends JFrame {
     }
 
     public void showSalesDateRangeReport(Date start, Date end) throws JRException, ClassNotFoundException, SQLException, IOException {
-
-        //String reportSrcFile = "data/Blank_A4.jrxml";
-        // First, compile jrxml file.
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/SalesReportA.jasper");
         // Fields for report
         param = setBasiParam();
-        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logo.png"));
+        File f = new File(MainAppController.B.getBLogo());
+        BufferedImage image = ImageIO.read(f);
         param.put("LOGO", image);
         param.put("SD", DateUtil.format3(start));
         param.put("ED", DateUtil.format3(end));
         param.put("UROLE", LoginController.u.getRoles());
-        //param.put("SP", LoginController.u.getRoles());
         SalesReportModel sm = new SalesReportModel(start, end);
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(sm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        // System.out.print("Done!");
-
     }
 
     public void showUserSalesDateRangeReport(Date start, Date end) throws JRException, ClassNotFoundException, SQLException, IOException {
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/SalesReportB.jasper");
         param = setBasiParam();
-        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logo.png"));
+        File f = new File(MainAppController.B.getBLogo());
+        BufferedImage image = ImageIO.read(f);
         param.put("LOGO", image);
         param.put("SD", DateUtil.format3(start));
         param.put("ED", DateUtil.format3(end));
@@ -178,77 +159,46 @@ public class PrintReport extends JFrame {
     public void showStockReorderReport() throws JRException, ClassNotFoundException, SQLException, IOException {
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/StockReorderLevelReport.jasper");
         param = setBasiParam();
-//        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle1.png"));
-//        BufferedImage image1 = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle2.png"));
-//        param.put("LOGO", image);
-//        param.put("LOGO1", image1);
         StockReorderReportModel sm = new StockReorderReportModel();
-        //Thread t = new Thread(sm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(sm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        // System.out.print("Done!");
 
     }
 
     public void showStocksReport() throws JRException, ClassNotFoundException, SQLException, IOException {
-
-        //String reportSrcFile = "data/Blank_A4.jrxml";
-        // First, compile jrxml file.
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/StockReport.jasper");
         // Fields for report
         param = setBasiParam();
-//        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle1.png"));
-//        BufferedImage image1 = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle2.png"));
-//        param.put("LOGO", image);
-//        param.put("LOGO1", image1);
         StockReportModel sm = new StockReportModel();
-        //Thread t = new Thread(sm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(sm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        //System.out.print("Done!");
-
     }
 
     public void showStocksDetailsReport() throws JRException, ClassNotFoundException, SQLException, IOException {
-
-        //String reportSrcFile = "data/Blank_A4.jrxml";
-        // First, compile jrxml file.
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/StockDetailsReport.jasper");
         // Fields for report
         param = setBasiParam();
-//        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle1.png"));
-//        BufferedImage image1 = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle2.png"));
-//        param.put("LOGO", image);
-//        param.put("LOGO1", image1);
         StockDetailsReportModel sm = new StockDetailsReportModel();
-        //Thread t = new Thread(sm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(sm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        //System.out.print("Done!");
 
     }
 
@@ -259,23 +209,15 @@ public class PrintReport extends JFrame {
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/ItemListReport.jasper");
         // Fields for report
         param = setBasiParam();
-//        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle1.png"));
-//        BufferedImage image1 = ImageIO.read(getClass().getResourceAsStream("/bt/resources/dodtitle2.png"));
-//        param.put("LOGO", image);
-//        param.put("LOGO1", image1);
         ItemListReportModel im = new ItemListReportModel();
-//        Thread t = new Thread(im);
-//        t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(im);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        //System.out.print("Done!");
 
     }
 
@@ -285,52 +227,35 @@ public class PrintReport extends JFrame {
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/DebtorsReport.jasper");
         // Fields for report
         param = setBasiParam();
-        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logo.png"));
+        File f = new File(MainAppController.B.getBLogo());
+        BufferedImage image = ImageIO.read(f);
         param.put("LOGO", image);
-        //param.put("SD", DateUtil.format3(start));
-        //param.put("ED", DateUtil.format3(end));
         DebtorsByCustomerReportModel dm = new DebtorsByCustomerReportModel(c);
-        //dm.dbname = c;
-        //Thread t = new Thread(dm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(dm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        //System.out.print("Done!");
-
     }
 
     public void showDebtorsBySalesReport() throws JRException, ClassNotFoundException, SQLException, IOException {
-
-        //String reportSrcFile = "data/Blank_A4.jrxml";
-        // First, compile jrxml file.
         InputStream inputStream = getClass().getResourceAsStream("/bt/bitsmartmini/reports/DebtorsReport.jasper");
         // Fields for report
         param = setBasiParam();
-        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/bt/resources/logo.png"));
+        File f = new File(MainAppController.B.getBLogo());
+        BufferedImage image = ImageIO.read(f);
         param.put("LOGO", image);
-        //param.put("SD", DateUtil.format3(start));
-        //param.put("ED", DateUtil.format3(end));
         DebtorsBySalesReportModel dm = new DebtorsBySalesReportModel();
-        //dm.customer = c;
-        //Thread t = new Thread(dm);
-        //t.start();
         JRTableModelDataSource jrtmds = new JRTableModelDataSource(dm);
         JasperPrint print = JasperFillManager.fillReport(inputStream, param, jrtmds);
         JRViewer viewer = new JRViewer(print);
         viewer.setOpaque(true);
         viewer.setVisible(true);
-//        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         this.add(viewer);
         this.setSize(dim);
         this.setVisible(true);
-        //System.out.print("Done!");
-
     }
 }
